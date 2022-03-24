c65ef68d2b79d1fc55a8de7b0b7221407ec55ec4
avadhari <abdullahvadhariya@gmail.com>
2022-03-24 19:47:43 -0400

    prefinal commit 3

--- akumar59/src/client.cpp
+++ akumar59/src/client.cpp
@@ -1,10 +1,6 @@
 #include "../include/client.h"
 #include "../include/logger.h"
-//#include <string.h>
-//#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
 #include <string>
